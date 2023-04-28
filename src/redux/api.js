bf66dac028dbd45dce634d560b15397cae2e85eb
Sunho Ju <sunho5515@gmail.com>
2023-04-29 05:50:03 +0900

    api.js baseURL 수정

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/sunho5810/WDTT/db',
+  baseURL: 'https://my-json-server.typicode.com/sunho5810/WDTT',
   // baseURL: 'http://localhost:5000',
   headers: {'X-Custom-Header': 'foobar'}
 });
